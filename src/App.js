55f8322c730da8f25b8274b261f1f927a3abadbd
csdear <stuart.dear@alloy.digital>
2021-09-15 15:36:20 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import Users from './component/users'
 import MovieMain from './component/MovieMain'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import Search from './component/Search'
 import Movie from './component/Movie';
 import axios from 'axios'
@@ -18,30 +16,29 @@ function App() {
   const [showSearchResults, setShowSearchResults] = useState(false);
 
   const search = async searchValue => {
-    console.log('HIT SEARCH function');
+    console.log('--- HIT SEARCH function');
     setLoading(true);
     setErrorMessage(null);
     setShowSearchResults(true);
     setShowArea(false);
 
     try {
-    const res = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e8c08c56c215123455db90ff4f2ce7f4&language=en-US&page=1&query=${searchValue}`)
-    .then(response => {
-      console.log('AXIOS RESPONSE', response);
-      setMovies(response.data.results);
-      setLoading(false);
-      });
-    }
-    catch(error){
-      setErrorMessage(error);
-      setLoading(false);
+      await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e8c08c56c215123455db90ff4f2ce7f4&language=en-US&page=1&query=${searchValue}`)
+      .then(res => {
+        console.log('--- AXIOS RESPONSE', res);
+        setMovies(res.data.results);
+        setLoading(false);
+        });
+      }
+      catch(error){
+        setErrorMessage(error);
+        setLoading(false);
 
-    }
-    }
+      }
+      }
 
-  const searchField = useRef(null)
+  // const searchField = useRef(null)
 
-  // let selection;
 
   const [state, setstate] = useState({
       filter:'',
@@ -55,18 +52,16 @@ function App() {
     setShowSearchResults(false);
   }
 
-  const setMovieSearch = () => {
-    const form = searchField.current
-    // alert(`${form['searchField'].value}`)
-    setstate({search: form['searchField'].value});
-    // console.log('AAA', state.searchString);
-  }
+  // const setMovieSearch = () => {
+  //   const form = searchField.current
+  //   console.log(`${form['searchField'].value}`) 
+  //   setstate({search: form['searchField'].value});
+  // }
 
 
   return (
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <h1>Tumble</h1>
       </header>
       <Button onClick={() => {setMovieFilter('trending');}} variant="warning">Trending</Button>
--- src/component/MovieMain.js
+++ src/component/MovieMain.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {getTrending} from '../store/actions/trendingAction'
 import {getTopRated} from '../store/actions/topRatedAction'
 import {getNowPlaying} from '../store/actions/nowPlayingAction'
 import {getPopular} from '../store/actions/popularAction'
-// import {searchMovies} from '../store/actions/searchAction'
+// import {searchMovies} from '../store/actions/searchAction' 
 import './styles/MovieMain.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -15,33 +15,28 @@ import Card from 'react-bootstrap/Card';
 
 const MovieMain = (props) => {
     console.log('@MovieMain');
-    console.log('PROPS',props);
+    console.log('--- PROPS',props);
     const filter = props.props.filter;
-    const search = props.props.search;
+    // const search = props.props.search;
     console.log('SELECTION filter',filter)
-    console.log('SELECTION search',search)
+    // console.log('SELECTION search',search)
+    
     const dispatch = useDispatch()
     const trendingList = useSelector(state => state.trendingList)
     const topRatedList = useSelector(state => state.topRatedList)
     const nowPlayingList = useSelector(state => state.nowPlayingList)
     const popularList = useSelector(state => state.popularList)
-    const movieList = useSelector(state => state.movieList)
-
-    
-
-
+    // const movieList = useSelector(state => state.movieList)
 
     let loading;
     let error;
     let list;
 
-    // init trending.
+    // init, load the  trending list.
     loading = trendingList.loading;
     error = trendingList.error;
     list = trendingList.list;
 
-
-    console.log('Current Filter selection', filter);
     switch (filter) {
         case 'trending':
           loading = trendingList.loading;
@@ -65,13 +60,6 @@ const MovieMain = (props) => {
           error = popularList.error;
           list = popularList.list;
           break;
-        // case undefined:
-        //   console.log('case undefined');
-        //   dispatch(searchMovies(search));
-        //   loading = movieList.loading;
-        //   error = movieList.error;
-        //   list = movieList.list;
-        //   break;
         default:
             console.log('case default');
             loading = trendingList.loading;
@@ -79,15 +67,14 @@ const MovieMain = (props) => {
             list = trendingList.list;
       }
 
-      if (search) {
-        console.log('|WE SEARCH|');
-        console.log('search Exists');
-        console.log('search is for : ', search);
-        // dispatch(searchMovies(search));  //WORKS BUT INFINITE LOOP.
+      // if (search) {
+      //   console.log('search exists, for : ', search);
+        // dispatch(searchMovies(search));  //WORKS BUT INFINITE LOOP!
+        // TRY HOOK useReducer Next Time.  
         // loading = movieList.loading;
         // error = movieList.error;
         // list = movieList.list;
-      }
+      // }
 
 
     //  useEffect(() => {
